6bd4a24307af84bf0a7718a7f0a5922ddb96cbf1
Harish-RC <164011988+Harish-RC@users.noreply.github.com>
2024-08-23 19:39:02 +0530

    Update remote_backup.sh

--- .github/workflows/scrpits/remote_backup.sh
+++ .github/workflows/scrpits/remote_backup.sh
@@ -5,7 +5,7 @@ source remote_backup.properties
 
 # SSH into the remote server and run the backup command
 ssh -t ${REMOTE_USER}@${REMOTE_HOST} << EOF
-  sudo su - mqm
+  . /opt/ibm/mqsi/iib-10.0.0.7/server/bin/mqsiprofile
   mqsibackupbroker $BROKER_NAME -a $BACKUP_FILE -d $BACKUP_DIR
 EOF
 
